7684f1f9d4967d1c2c908043b45130e4113fbf97
wangpeng <1160440673@qq.com>
2018-07-18 22:06:55 +0800

    2018年7月18日 22:06

--- src/main/java/com/wp/config/WebConfiguration.java
+++ src/main/java/com/wp/config/WebConfiguration.java
@@ -1,7 +1,7 @@
 package com.wp.config;
 
 import org.apache.catalina.filters.RemoteIpFilter;
-import org.springframework.boot.context.embedded.FilterRegistrationBean;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,19 +10,19 @@ import com.wp.filter.MyFilter;
 @Configuration
 public class WebConfiguration {
 
-	@Bean
-	public RemoteIpFilter remoteIpFilter(){
-		return new RemoteIpFilter();
-	}
-	
-	@Bean
-	public FilterRegistrationBean testFilterRegistration(){
-		FilterRegistrationBean bean = new FilterRegistrationBean();
-		bean.setFilter(new MyFilter());
-		bean.addUrlPatterns("/*");
-		bean.addInitParameter("paramName", "paramValue");
-		bean.setName("MyFilter");
-		bean.setOrder(1);
-		return bean;
-	} 
+//	@Bean
+//	public RemoteIpFilter remoteIpFilter(){
+//		return new RemoteIpFilter();
+//	}
+//
+//	@Bean
+//	public FilterRegistrationBean testFilterRegistration(){
+//		FilterRegistrationBean bean = new FilterRegistrationBean();
+//		bean.setFilter(new MyFilter());
+//		bean.addUrlPatterns("/*");
+//		bean.addInitParameter("paramName", "paramValue");
+//		bean.setName("MyFilter");
+//		bean.setOrder(1);
+//		return bean;
+//	}
 }
---
+++ src/main/java/com/wp/controller/ReaderLineController.java
@@ -0,0 +1,26 @@
+package com.wp.controller;
+
+import com.wp.dao.Repository;
+import com.wp.model.Book;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/")
+public class ReaderLineController{
+    @Autowired
+    private Repository repository;
+
+    @RequestMapping(value="/{reader}", method = RequestMethod.GET)
+    public String readerBook(@PathVariable("reader") String reader, Model model){
+        List<Book> list = repository.findByReader(reader);
+        model.addAttribute("books", list);
+        return "readingList";
+    }
+}
