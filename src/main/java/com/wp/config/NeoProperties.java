693f9c6c08b03e8353b4ab3d4d0d2c334c78512d
wangpeng <1160440673@qq.com>
2018-07-19 08:30:53 +0800

    提交

---
+++ src/main/java/com/wp/config/NeoProperties.java
@@ -0,0 +1,24 @@
+package com.wp.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class NeoProperties {
+//	@Value("${com.neo.title}")
+//	private String title;
+//	@Value("${com.neo.des}")
+//	private String desc;
+//	public String getTitle() {
+//		return title;
+//	}
+//	public void setTitle(String title) {
+//		this.title = title;
+//	}
+//	public String getDesc() {
+//		return desc;
+//	}
+//	public void setDesc(String desc) {
+//		this.desc = desc;
+//	}
+}
