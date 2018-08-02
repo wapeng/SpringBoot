690fa2457f48ebd526d2c6fb65214ca449e43b05
wangpeng <1160440673@qq.com>
2018-08-02 16:10:38 +0800

    同步

--- src/main/java/com/wp/controller/ReaderLineController.java
+++ src/main/java/com/wp/controller/ReaderLineController.java
@@ -1,7 +1,5 @@
 package com.wp.controller;
 
-import com.wp.dao.Repository;
-import com.wp.model.Book;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
